6ba0bbbdbf1b2097ab47901bba269c55af5d5c8e
tsu-nera <fox10225fox@gmail.com>
2014-11-11 20:13:00 +0900

    chapter7 done

--- src/tdd_by_example/Doller.java
+++ src/tdd_by_example/Doller.java
@@ -1,8 +1,6 @@
 package tdd_by_example;
 
-public class Doller {
-
-	private int amount;
+public class Doller extends Money {
 
 	public Doller(int amount) {
 		this.amount = amount;
@@ -11,12 +9,5 @@ public class Doller {
 	public Doller times(int multiplier) {
 		return new Doller(amount * multiplier);
 	}
-
-	public boolean equals(Object object) {
-		Doller doller = (Doller) object;
-		return amount == doller.amount;
-	}
-
-	
 
 }
