3736d82e8ede9ff48f31a54f348a8be502840ef4
tsu-nera <fox10225fox@gmail.com>
2014-11-11 20:03:19 +0900

    chapter5 done

--- test/tdd_by_example/DollerTest.java
+++ test/tdd_by_example/DollerTest.java
@@ -17,5 +17,12 @@ public class DollerTest {
 	public void testEquality() {
 		assertTrue(new Doller(5).equals(new Doller(5)));
 	}
+
+	@Test
+	public void testFrancMultiplication() {
+		Franc five = new Franc(5);
+		assertEquals(new Franc(10), five.times(2));
+		assertEquals(new Franc(15), five.times(3));		
+	}
 }
 
